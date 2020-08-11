659e465d80f4f3660ce52603b5442f3d843e59a8
maumainsaw <maumainsaw@gmail.com>
2020-08-10 23:31:11 -0500

    Calculando costo por marca con un helper con functions

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,18 @@
+//diferencia de years
+
 export function obtenerDiferenciaYear(year){
     return new Date().getFullYear()-year;
+}
+
+//calcula el total a pagar segun la amrca
+export function calcularMarca(marca){
+    let incremento;
+
+    switch(marca){
+        case 'europeo':incremento=1.30; break;
+        case 'americano':incremento=1.15; break;
+        case 'asiatico':incremento=1.05;break;
+        default:break;
+    }
+    return incremento;
 }
