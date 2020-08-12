fa861bde4f2f11f41a60072e4f2cf6d5059d32c0
maumainsaw <maumainsaw@gmail.com>
2020-08-11 21:54:33 -0500

    Calculando costo basado en plan

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import styled from '@emotion/styled'
-import {obtenerDiferenciaYear,calcularMarca} from '../helper'
+import {obtenerDiferenciaYear,calcularMarca,obtenerPlan} from '../helper'
 
 const Campo = styled.div`
     display:flex;
@@ -89,18 +89,20 @@ const cotizarSeguro =e =>{
 
     //por cada year restar el 3%
     resultado -=( (diferencia*3)*resultado)/100;
-    console.log(resultado);
+
 
     //Americano 15%
     //Asiatico 5%
     //Europeo 30%
     resultado =calcularMarca(marca)*resultado;
-    console.log(resultado)
+   
 
 
     //Basico aumenta 20%
-
     //Completo 50%
+    const incrementoPlan =obtenerPlan(plan);
+    resultado = parseFloat(incrementoPlan*resultado).toFixed(2);
+    
 
 
     //Total
